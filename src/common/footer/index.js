86c353b77ce2fa20e4ef88cee59945b45dbbe4e8
吴晓权 <somerq@qq.com>
2019-04-05 17:52:58 +0800

    feature: 备案链接小优化

--- src/common/footer/index.js
+++ src/common/footer/index.js
@@ -4,5 +4,5 @@ import {
 } from './style';
 
 export default function Footer () {
-  return <FooterWrapper>Copyright&copy;2018-2019 吴晓权 版权所有 | 备案号：<a target="_blank" href="http://www.miitbeian.gov.cn">粤ICP备18084360号-1</a></FooterWrapper>
+  return <FooterWrapper>Copyright&copy;2018-2019 吴晓权 版权所有 | 备案号：<a target="_blank" rel="noopener noreferrer" href="http://www.miitbeian.gov.cn">粤ICP备18084360号-1</a></FooterWrapper>
 }
